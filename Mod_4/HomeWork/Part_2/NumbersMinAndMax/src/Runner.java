2e35803d9b662693b12b03904989fea41226ddb1
volodya.chabak <volodya.chabak@gmail.com>
2019-12-02 14:58:45 +0200

    Commit Mod-4 Part 2

---
+++ Mod_4/HomeWork/Part_2/NumbersMinAndMax/src/Runner.java
@@ -0,0 +1,12 @@
+public class Runner {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Byte: min = %d, max = %d\n", Byte.MIN_VALUE, Byte.MAX_VALUE);
+        System.out.printf("Short: min = %d, max = %d\n", Short.MIN_VALUE, Short.MAX_VALUE);
+        System.out.printf("Integer: min = %d, max = %d\n", Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.printf("Long: min = %d, max = %d\n", Long.MIN_VALUE, Long.MAX_VALUE);
+        System.out.println("Float: min = " + Float.MIN_VALUE + ", max = " + Float.MAX_VALUE);
+        System.out.println("Double: min = " + Double.MIN_VALUE + ", max = " + Double.MAX_VALUE);
+    }
+}
